207baca772c603e8e405b167df48d5fe5db7804f
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-07 22:54:42 +0100

    Resolved a bug

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Layout from "../components/layout";
+import Heading from "../components/heading"
+import LeftPane from "../components/home/LeftPane";
+import RightPane from "../components/home/RightPane";
 const Index = ({}) => {
   return (
     <>
       <Heading title={"NewsLetter Sign Up"} />
       <Layout>
-        
+        <div className="row">
+          <div className="col">
+            <LeftPane />
+          </div>
+          <div className="col">
+            <RightPane />
+          </div>
+        </div>
       </Layout>
     </>
   );
