cdc425f62694e56eed6b64575ca898c74c009b3e
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-07 23:53:10 +0100

    Removed wrongly uploaded files

--- components/newsletter-sign-up/newsletter-layout.js
+++ components/newsletter-sign-up/newsletter-layout.js
@@ -5,6 +5,20 @@ const NewsLetterLayout = ({ children }) => {
     <>
       <div className={`${styles.fontFaceRobotoRegular} ${styles.body}`}>
         <main>{children}</main>
+        <div className="text-center" style={{ color: "#ffffff" }}>
+          Challenge by{" "}
+          <a
+            target="_blank"
+            href="https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a target="_blank" href="https://github.com/inioluwabello">
+            David Ini
+          </a>
+          .
+        </div>
       </div>
     </>
   );
