3d75c1e1fa569f54ec2b117852955c437a196079
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-08 14:58:13 +0100

    Added LinkedIn

--- pages/works.js
+++ pages/works.js
@@ -4,17 +4,32 @@ const Works = () => {
   return (
     <>
       <div className="projects p-3">
-        <div id="project-1">
-          <Link href="https://newsletter-nine-ecru.vercel.app">Newsletter Sign Up</Link>
+        <div id="project-0">
+          <Link href="https://linkedin-two-chi.vercel.app">
+            LinkedIn Clone
+          </Link>
           <p>
-            Design preview for the Frontend Mentor - "Newsletter sign-up" coding challenge
+            A clone of the LinkeIn Feeds page
+          </p>
+        </div>
+
+        <div id="project-1">
+          <Link href="https://newsletter-nine-ecru.vercel.app">
+            Newsletter Sign Up
+          </Link>
+          <p>
+            Design preview for the Frontend Mentor - "Newsletter sign-up" coding
+            challenge
           </p>
         </div>
 
         <div id="project-2">
-          <Link href="https://age-calculator-pi-one.vercel.app">Age Calculator</Link>
+          <Link href="https://age-calculator-pi-one.vercel.app">
+            Age Calculator
+          </Link>
           <p>
-            Design preview for the FrontEnd Mentor Age Calculator coding challenge
+            Design preview for the FrontEnd Mentor Age Calculator coding
+            challenge
           </p>
         </div>
       </div>
