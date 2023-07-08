f4c4e2708191cb31e584db92001af4629243cc95
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-08 14:37:30 +0100

    Update to works

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # inis-portfolio-app
 
 Portfolio project containing the following apps:
-Newsletter
-- [GitHubSolution](https://github.com/inioluwabello/newsletter)
-- [LiveSolution](https://github.com/inioluwabello/newsletter)
+----------------------------------------------------------------------------
+
+### Newsletter
+- [GitHub Solution](https://github.com/inioluwabello/newsletter)
+- [Live Solution](https://newsletter-nine-ecru.vercel.app)
+
+### Age Calculator
+- [GitHub Solution](https://github.com/inioluwabello/age-calculator)
+- [Live Solution](https://age-calculator-pi-one.vercel.app)
--- pages/works.js
+++ pages/works.js
@@ -5,10 +5,16 @@ const Works = () => {
     <>
       <div className="projects">
         <div id="project-1">
-          <Link href="/newsletter-sign-up">Newsletter Sign Up</Link>
+          <Link href="https://newsletter-nine-ecru.vercel.app">Newsletter Sign Up</Link>
           <p>
-            Design preview for the Newsletter sign-up form with success message
-            coding challenge
+            Design preview for the Frontend Mentor - "Newsletter sign-up" coding challenge
+          </p>
+        </div>
+
+        <div id="project-2">
+          <Link href="https://age-calculator-pi-one.vercel.app">Age Calculator</Link>
+          <p>
+            Design preview for the FrontEnd Mentor Age Calculator coding challenge
           </p>
         </div>
       </div>
